939b3cfeb95ba33fee72ed6c10d8ab4a3071d00a
Arthur Melton <amtitan@icloud.com>
2021-04-24 19:20:09 -0400

    simple database

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,19 @@ require("dotenv").config();
 const { Client } = require('discord.js');
 const client = new Client();
 const PREFIX = "D&D!";
+const fs = require("fs");
+
+try {
+    if(fs.existsSync('data.json')) {
+        console.log("The file exists.");
+    } else {
+        fs.writeFile('data.json', '{}', function (err) {
+			if (err) return console.log(err);
+		});
+    }
+} catch (err) {
+    console.error(err);
+}
 
 client.on('ready', () => {
 	console.log(`${client.user.tag} bot is on`);
@@ -15,6 +28,18 @@ client.on('message', (message) => {
 	if (!message.content.toLowerCase().startsWith(PREFIX.toLowerCase())) return;
 	if (message.guild && !message.guild.me.permissionsIn(message.channel.id).any("SEND_MESSAGES")) return;
 	console.log(`[${new Date}]: ${message.content}`);
+	const parseData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    if (!parseData.hasOwnProperty(message.author.id)) {
+    	parseData[message.author.id] = null;
+    	const data = JSON.stringify(parseData);
+		fs.writeFile('data.json', data, 'utf8', (err) => {
+		    if (err) {
+		        console.log(`Error writing file: ${err}`);
+		    } else {
+		        console.log(`File is written successfully!`);
+		    }
+		});
+    }
 	const [commandName, ...args] = message.content.toLowerCase()
 		.trim()
 		.substring(PREFIX.length)
@@ -50,5 +75,9 @@ client.on('message', (message) => {
 		});
 	}
 })
+
+function dice() {
+	return Math.round(Math.random() * 6);
+}
 
 client.login(process.env.Token);
